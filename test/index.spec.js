c32c51bf15255bee62fd0c07c647c04cf202999f
Tomislav <tomislav.habalija@gmail.com>
2018-07-06 19:15:32 +0200

    Added tests for use dashes

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,98 +6,130 @@ let expect = chai.expect;
 
 describe('Function getPassphrase(numberOfWords, useDashes)', () => {
     it('should return string', () => {
-        let passphrase = passphraseGeneratorHR.getPassphrase();
+        const passphrase = passphraseGeneratorHR.getPassphrase();
         expect(passphrase).to.be.a('string');
     });
     it('should return right amount of words', () => {
         const numberOfWords = 5;
-        let passphrase = passphraseGeneratorHR.getPassphrase(numberOfWords);
-        let wordArray = passphrase.split(' ');
+        const passphrase = passphraseGeneratorHR.getPassphrase(numberOfWords);
+        const wordArray = passphrase.split(' ');
         expect(wordArray.length).to.equal(numberOfWords);
     });
 });
 
 describe('Function getSimplePassphrase(numberOfWords, useDashes)', () => {
     it('should return string', () => {
-        let passphrase = passphraseGeneratorHR.getSimplePassphrase();
+        const passphrase = passphraseGeneratorHR.getSimplePassphrase();
         expect(passphrase).to.be.a('string');
     });
     it('should return right amount of words', () => {
         const numberOfWords = 5; 
-        let passphrase = passphraseGeneratorHR.getSimplePassphrase(numberOfWords);
-        let wordArray = passphrase.split(' ');
+        const passphrase = passphraseGeneratorHR.getSimplePassphrase(numberOfWords);
+        const wordArray = passphrase.split(' ');
         expect(wordArray.length).to.equal(numberOfWords);
     });
     it('should return passphrase consisting of words (3-5 letters per word)', () => {
-        let passphrase = passphraseGeneratorHR.getSimplePassphrase();
-        let wordArray = passphrase.split(' ');
-        let index = wordArray.findIndex(word => {
+        const passphrase = passphraseGeneratorHR.getSimplePassphrase();
+        const wordArray = passphrase.split(' ');
+        const index = wordArray.findIndex(word => {
             return word.length > 5 || word.length < 3
         })
         expect(index).to.equal(-1);
+    });
+    it('should return passphrase consisting of words devided by dashes', () => {
+        const numberOfWords = 5; 
+        const useDashes = true;
+        const passphrase = passphraseGeneratorHR.getSimplePassphrase(numberOfWords, useDashes);
+        const wordArray = passphrase.split('-');
+        expect(wordArray.length).to.equal(numberOfWords);
     });
 });
 
 describe('Function getStrongPassphrase(numberOfWords, useDashes)', () => {
     it('should return string', () => {
-        let passphrase = passphraseGeneratorHR.getStrongPassphrase();
+        const passphrase = passphraseGeneratorHR.getStrongPassphrase();
         expect(passphrase).to.be.a('string');
     });
     it('should return right amount of words', () => {
         const numberOfWords = 5; 
-        let passphrase = passphraseGeneratorHR.getStrongPassphrase(numberOfWords);
-        let wordArray = passphrase.split(' ');
+        const passphrase = passphraseGeneratorHR.getStrongPassphrase(numberOfWords);
+        const wordArray = passphrase.split(' ');
         expect(wordArray.length).to.equal(numberOfWords);
     });
     it('should return passphrase consisting of words (6-8 letters per word)', () => {
-        let passphrase = passphraseGeneratorHR.getStrongPassphrase();
-        let wordArray = passphrase.split(' ');
-        let index = wordArray.findIndex(word => {
+        const passphrase = passphraseGeneratorHR.getStrongPassphrase();
+        const wordArray = passphrase.split(' ');
+        const index = wordArray.findIndex(word => {
             return word.length < 6 || word.length > 8
         })
         expect(index).to.equal(-1);
+    });
+    it('should return passphrase consisting of words devided by dashes', () => {
+        const numberOfWords = 5; 
+        const useDashes = true;
+        const passphrase = passphraseGeneratorHR.getStrongPassphrase(numberOfWords, useDashes);
+        const wordArray = passphrase.split('-');
+        expect(wordArray.length).to.equal(numberOfWords);
     });
 });
 
 describe('Function getUltimatePassphrase(numberOfWords, useDashes)', () => {
     it('should return string', () => {
-        let passphrase = passphraseGeneratorHR.getUltimatePassphrase();
+        const passphrase = passphraseGeneratorHR.getUltimatePassphrase();
         expect(passphrase).to.be.a('string');
     });
     it('should return right amount of words', () => {
         const numberOfWords = 5; 
-        let passphrase = passphraseGeneratorHR.getUltimatePassphrase(numberOfWords);
-        let wordArray = passphrase.split(' ');
+        const passphrase = passphraseGeneratorHR.getUltimatePassphrase(numberOfWords);
+        const wordArray = passphrase.split(' ');
         expect(wordArray.length).to.equal(numberOfWords);
     });
     it('should return passphrase consisting of words (9+ letters per word)', () => {
-        let passphrase = passphraseGeneratorHR.getUltimatePassphrase();
-        let wordArray = passphrase.split(' ');
-        let index = wordArray.findIndex(word => {
+        const passphrase = passphraseGeneratorHR.getUltimatePassphrase();
+        const wordArray = passphrase.split(' ');
+        const index = wordArray.findIndex(word => {
             return word.length < 9
         })
         expect(index).to.equal(-1);
+    });
+    it('should return passphrase consisting of words devided by dashes', () => {
+        const numberOfWords = 5; 
+        const useDashes = true;
+        const passphrase = passphraseGeneratorHR.getUltimatePassphrase(numberOfWords, useDashes);
+        const wordArray = passphrase.split('-');
+        expect(wordArray.length).to.equal(numberOfWords);
     });
 });
 
 describe('Function generatePassphrase(numberOfWords, useDashes, wordArrays)', () => {
     it('should return string', () => {
-        let numberOfWords = 1;
-        let wordArrays = [['word'], ['word']]
-        let passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, false, wordArrays);
+        const numberOfWords = 1;
+        const useDashes = true;
+        const wordArrays = [['word'], ['word']]
+        const passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, useDashes, wordArrays);
         expect(passphrase).to.be.a('string');
     });
     it('should return right amount of words', () => {
-        let numberOfWords = 1;
-        let wordArrays = [['word'], ['word']]
-        let passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, false, wordArrays);
-        let wordArray = passphrase.split(' ');
+        const numberOfWords = 1;
+        const useDashes = true;
+        const wordArrays = [['word'], ['word']]
+        const passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, useDashes, wordArrays);
+        const wordArray = passphrase.split(' ');
         expect(wordArray.length).to.equal(numberOfWords);
     });
     it('should return right result', () => {
-        let numberOfWords = 1;
-        let wordArrays = [['word']]
-        let passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, false, wordArrays);
+        const numberOfWords = 1;
+        const useDashes = true;
+        const wordArrays = [['word']]
+        const passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, useDashes, wordArrays);
         expect(passphrase).to.equal(wordArrays[0][0]);
+    });
+    it('should return passphrase consisting of words devided by dashes', () => {
+        const numberOfWords = 2;
+        const useDashes = true;
+        const wordArrays = [['word'], ['word']]
+        const passphrase = passphraseGeneratorHR.generatePassphrase(numberOfWords, useDashes, wordArrays);
+        const wordArray = passphrase.split('-');
+        expect(wordArray.length).to.equal(numberOfWords);
     });
 });
